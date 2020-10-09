b64920d575cb1d2b771788ad3c5ec0148a9b5490
Yuri Range <ysr7santos@gmail.com>
2020-10-09 20:01:16 -0300

    Ajustes no gitignore  e ImageConfiguration

--- 04_InfraStructure/Repository/Configurations/ImageConfiguration.cs
+++ 04_InfraStructure/Repository/Configurations/ImageConfiguration.cs
@@ -1,11 +1,12 @@
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Repository.Configurations
 {
 
     public class ImageConfiguration : IEntityTypeConfiguration<Image>
     {
-
         public void Configure(EntityTypeBuilder<Image> builder)
         {
             builder.HasKey(item => item.Id);
--- 04_InfraStructure/Repository/Repositories/ImageRepository.cs
+++ 04_InfraStructure/Repository/Repositories/ImageRepository.cs
@@ -3,7 +3,7 @@ using Core.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace Repository.Repositories {
-    public class ImageRepository : BaseRepository<Image> 
+    public class ImageRepository : BaseRepository<Image>, IImageRepository
     {
 
         public ImageRepository (DbSet<Image> entity, BaseContext context)
