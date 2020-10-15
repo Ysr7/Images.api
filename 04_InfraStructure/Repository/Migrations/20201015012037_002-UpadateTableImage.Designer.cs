5e937a8f6056dc063fefb180cb8589c7f4e7333c
Yuri Range <ysr7santos@gmail.com>
2020-10-14 23:05:07 -0300

    Migration para incerção de novos campos na tabela de Images

--- 04_InfraStructure/Repository/Migrations/20201015012037_002-UpadateTableImage.Designer.cs
+++ 04_InfraStructure/Repository/Migrations/20201015012037_002-UpadateTableImage.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
 namespace Repository.Migrations
 {
     [DbContext(typeof(BaseContext))]
-    partial class BaseContextModelSnapshot : ModelSnapshot
+    [Migration("20201015012037_002-UpadateTableImage")]
+    partial class _002UpadateTableImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ 04_InfraStructure/Repository/Migrations/20201015012037_002-UpadateTableImage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class _002UpadateTableImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Length",
+                table: "Images",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Images",
+                maxLength: 155,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Length",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
